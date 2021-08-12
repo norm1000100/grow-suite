f5ca35348df64f707af19f30bc7f532957616db4
Braeden V <norm1000100@gmail.com>
2021-08-11 17:20:47 -0700

    definitions.h for global variables added

---
+++ Grow light/include/definitions.h
@@ -0,0 +1,15 @@
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+/*
+Definitions for global use.
+Change to suit the needs of your board
+*/
+
+
+
+extern const int relay = 0; // pin used to enable the light
+extern const int status_led = 2; // LED pin to determine program status 
+extern const int baud_rate = 9600; // baud rate of device for serial communication
+
+#endif
